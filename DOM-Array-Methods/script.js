1ebda3aa209287e0f5d248fbea645ff1960fc1e7
Boppuri <tony.villiams@gmail.com>
2020-05-31 22:07:08 +0100

    DAM: getting the user details

---
+++ DOM-Array-Methods/script.js
@@ -0,0 +1,33 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showBillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// Fetch Randon user and money
+async function getRandomUser() {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+
+  // console.log(data);
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+
+  addData(newUser);
+}
+
+// Add new obj to data arr
+function addData(obj) {
+  data.push(obj);
+}
