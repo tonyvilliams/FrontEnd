e9c863a6d5341c8608f8b103541d6b010eb92034
Boppuri <tony.villiams@gmail.com>
2020-05-31 14:00:14 +0100

    CVP: setup evt listerners

---
+++ CustomVideoPlayer/script.js
@@ -0,0 +1,15 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+// Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+stop.addEventListener('click', toggleVideoStatus);
+progress.addEventListener('change', setVideoProgress);
